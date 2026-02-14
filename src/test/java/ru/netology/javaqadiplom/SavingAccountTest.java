753c52e9df016b4b633de830cd67d37280860012
Александр Саломанин <alexzsanjes@gmail.com>
2026-02-14 21:19:56 +0700

    saving tested

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -3,7 +3,102 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SavingAccountTest {
+
+    @Test
+    public void shouldThrowExceptionWhenMinBalanceIsNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(
+                    2_000,
+                    -1_000,
+                    10_000,
+                    5
+            );
+        });
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenInitialBalanceLessThenMin() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(
+                    0,
+                    500,
+                    10_000,
+                    5
+            );
+        });
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenMinBalanceMoreThanMax() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(
+                    2_000,
+                    12_000,
+                    10_000,
+                    5
+            );
+        });
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenRateIsNegative() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new SavingAccount(
+                    2_000,
+                    1_000,
+                    10_000,
+                    -5
+            );
+        });
+
+    }
+
+    @Test
+    public void shouldPayWhenEqualsMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(2_000 - 1_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayLessThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_500);
+
+        Assertions.assertEquals(2000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayMoreThanMinBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(500);
+
+        Assertions.assertEquals(2000 - 500, account.getBalance());
+    }
 
     @Test
     public void shouldAddLessThanMaxBalance() {
@@ -17,5 +112,56 @@ public class SavingAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(9_000);
+
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldAddEqualsMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_000);
+
+        Assertions.assertEquals(2_000 + 8_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldCalculatePercentOnTheBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                15
+        );
+
+        Assertions.assertEquals(300,account.yearChange());
+    }
+    @Test
+    public void shouldCalculatePercentOnTheMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                10_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(0,account.yearChange());
     }
 }
