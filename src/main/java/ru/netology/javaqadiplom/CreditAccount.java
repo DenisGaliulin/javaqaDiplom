97872fc6d1c4c29105d63970eb8a631b574f2f2f
Denis.test <denis.galiulin.a@yandex.ru>
2026-02-15 17:27:23 +0500

    fixed the CreditAccount

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -80,7 +80,7 @@ public class CreditAccount extends Account {
      */
     @Override
     public int yearChange() {
-        return balance / 100 * rate;
+        return (int)(long)balance / 100 * rate;
     }
 
     public int getCreditLimit() {
